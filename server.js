08d7e9d4e140d1e489a1551d1d9cd2d2ad5b8813
MohanraamS15 <mohanraams15@gmail.com>
2026-05-31 23:34:44 +0530

    Completed realtime chat application

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import crypto from 'crypto';
 const app = express();
 
 app.use(express.json());
-app.use(express.static("."));
+app.use(express.static("./src"));
 
 const server = createServer(app);
 
